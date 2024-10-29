b5c80e0791cadaacb41059cca978c6c120b1ef5c
Giau Nguyen <giaunguyen@air-de-giau.home>
2024-10-29 22:01:40 +0100

    Ajout des question.js et game.js

--- game.js
+++ game.js
@@ -1,16 +1,25 @@
-import { quiz_fatoumata_kebe } from './question.js'; // Import des questions
+import {quizZilla} from './question.js' // Import des questions
 
 // Récupérer les emplacements pour injecter la question et les options
 const premiereQuestion = document.querySelector('.question')
-const optionPremierQuestion = document.querySelector('.options1')
+const optionContainer = document.querySelector('.options')
 
 // Récupérer la première question
-const firstQuestion = __________.questions[0];
+const firstQuestion = quizZilla.questions[0];
 
 // Injecter le texte de la question dans l'emplacement dédié
-__________.innerText = __________;
+premiereQuestion.innerText = firstQuestion.text;
 
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
+
+firstQuestion.options.forEach(option => {
+  const optionButton = document.createElement('button'); // crée l'élément button html <button></button>
+  optionButton.innerText = option; // on change le text de button <button> le text de option </button>
+  //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html
+  // optionButton.classList.add("pinguoin"); 
+  optionContainer.appendChild(optionButton); // on ajoute le button comme enfant de optionPremierQuestion
+});
+
 firstQuestion.__________.forEach(__________ => {
   const __________ = document.createElement('button');
   __________.innerText = __________;
