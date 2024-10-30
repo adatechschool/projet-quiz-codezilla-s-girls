7c71c4645cb9c2475aaf4fb6fdaab58ab137534c
AssiarDev <halibaraissa@gmail.com>
2024-10-30 15:35:43 +0100

    resolution du bugg

--- game.js
+++ game.js
@@ -1,61 +1,43 @@
 import { quizZilla } from './question.js'; // Import des questions
 
+console.log(quizZilla)
 // Récupérer les emplacements pour injecter la question et les options
 const questionsElement = document.querySelector('.question');
 const optionsQuestionElement = document.querySelector('.options');
 
 // Récupérer la liste des questions question 
 let questionsList = quizZilla.questions;
-
-// Récuperer les options d'une question
-const optionQuestions = quizZilla.questions[0].options;
-console.log("Les options de réponses sont :", optionQuestions);
-
-optionQuestions.forEach( option => {
-  //console.log(option)
-  const optionElement = document.createElement('button');
-  optionElement.innerText = option;
-  optionsQuestionElement.appendChild(optionElement);
-});
+console.log("je recupere les questions", questionsList)
 
 let currentIndex = 0;
 
 // Créer une fonction qui affciche dynamiquement les questions
 const displayQuestions = (index) => {
+  const question = questionsList[index]
+  questionsElement.innerText = question.text;
+
   optionsQuestionElement.innerHTML = ''
 
-  const question = questionsList[index]
-  //console.log("affiche moi les questions :", question.text)
-  questionsElement.innerText = question.text;
-  //console.log("in displayQuestion", questionsElement)
-
-  optionQuestions.forEach( option => {
-    //console.log(option)
+  question.options.forEach( option => {
     const optionElement = document.createElement('button');
     optionElement.innerText = option;
     optionsQuestionElement.appendChild(optionElement);
   });
 };
 
-//displayQuestions(currentIndex);
-
 // Récupérer le bouton suivant
 const nextButton = document.getElementById('next-button');
 
-const loadQuestion = () => {
-  // Afficher une question en fonction de l'index
-  nextButton.addEventListener('click', () => {
-    currentIndex++;
-    console.log(currentIndex)
-    if (currentIndex < quizZilla.questions.length){
-      displayQuestions(currentIndex)
-    } else {
-      questionsElement.innerText = "Plus de question"
-      optionQuestions.innerHTML = ''
-    }
-  })
+nextButton.addEventListener('click', () => {
+  currentIndex++;
+  console.log(currentIndex)
+  if (currentIndex < quizZilla.questions.length){
+    displayQuestions(currentIndex)
+  } else {
+    questionsElement.innerText = "Plus de question"
+    optionsQuestionElement.innerHTML = ''
+  }
+})
 
-}
 
-loadQuestion()
-displayQuestions(currentIndex)
+displayQuestions(currentIndex);
