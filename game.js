49463d98a5e0ed589c1671ef42d52000568fe754
Giau Nguyen <giaunguyen@MacBook-Air-de-Giau.local>
2024-10-31 12:58:26 +0100

    Supprimer les commentaires

--- game.js
+++ game.js
@@ -8,29 +8,20 @@ const optionContainer = document.querySelector('.options')
 const firstQuestion = quizZilla.questions[0];
 
 // Injecter le texte de la question dans l'emplacement dédit questionContainer.innerText = firstQuestion.text;
-
+questionContainer.innerText = firstQuestion.text
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
-
-firstQuestion.options.forEach(option => {
+  firstQuestion.options.forEach(option => {
   const optionButton = document.createElement('button'); // crée l'élément button html <button></button>
   optionButton.innerText = option; // on change le text de button <button> le text de option </button>
-  //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html
-  // optionButton.classList.add("pinguoin"); 
+  //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html 
   optionContainer.appendChild(optionButton); // on ajoute le button comme enfant de optionContainer
 }); 
-
-
 // Variables pour suivre l'état du quiz
 let currentQuestionIndex = 0; // Commence à la première question
 
 // Sélection des éléments HTML
 const buttonSuivant = document.querySelector('#next-button');
-//const __________ = document.getElementById('__________');
-//const __________ = document.getElementById('__________');
 
-// Fonction pour afficher une question basée sur l'index actuel
-
-//function loadQuestion() {
 function loadQuestion(currentQuestionIndex){
   // Vider le conteneur des options
   optionContainer.innerHTML = '';
@@ -40,14 +31,14 @@ function loadQuestion(currentQuestionIndex){
   // Injecter la question dans le HTML
   questionContainer.innerText = currentQuestion.text;
   // Injecter les options dans le HTML 
+  
   currentQuestion.options.forEach(option => {
     const optionButton = document.createElement('button');
-    optionButton.innerText = option;
-   // __________.classList.add('__________');
+    optionButton.innerText = option; 
+    // optionContainer.classList.add('option-button');
     optionContainer.appendChild(optionButton);
-  });
-  }
-  //function loadQuestion() {
+  })
+}
 // Ajouter un écouteur d'événements pour le bouton "Suivant"
 buttonSuivant.addEventListener('click',() => {
   // Incrémenter l'index de la question
@@ -56,10 +47,10 @@ buttonSuivant.addEventListener('click',() => {
   if (currentQuestionIndex < quizZilla.questions.length) {
     // Afficher la question suivante 
     loadQuestion(currentQuestionIndex)
-    //currentQuestionIndex++
+  
   } else {
     // Si plus de questions, indiquer la fin du quiz
-  questionContainer.innerText = 'Fin du quiz';
+    questionContainer.innerText = 'Fin du quiz';
     optionContainer.innerHTML = ''; // Effacer les options
     buttonSuivant.style.display = 'none'; // Cacher le bouton Suivant
     replayButton.style.display="inline-block"
@@ -75,8 +66,20 @@ replayButton.addEventListener('click', () => {
     buttonSuivant.style.display='inline-block'
     replayButton.style.display ='none'
     loadQuestion(currentQuestionIndex)
+})
   
   // TODO Cacher le bouton Rejouer et afficher le bouton Suivant
   
   // TODO Recharger la première question 
-});
+  function checkAnswer() {
+    optionButton.addEventListener("click",(event)=>
+    {
+      const optionChoisi = event.target.innerText
+    if (optionChoisi === currentQuestion.correct_answer) {
+      alert("correct"); 
+    } else {
+      alert("incorrect"); 
+    }
+  })
+  }
+  checkAnswer()
