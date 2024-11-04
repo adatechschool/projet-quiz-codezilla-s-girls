8402c569ddf4e3f688a56a572c8f7d1b1292e636
stellab95 <stellab95@hotmail.fr>
2024-11-04 16:24:00 +0100

    fichiers à jour

--- game.js
+++ game.js
@@ -1,98 +1,108 @@
-import {quizZilla} from './question.js' // Import des questions
+import { quizZilla } from './question.js';
+
 // Récupérer les emplacements pour injecter la question et les options
-const questionContainer = document.querySelector('.question')
-const optionContainer = document.querySelector('.options')
+const questionContainer = document.querySelector('.question');
+const optionContainer = document.querySelector('.options');
 
-// Récupérer la première question
-/*const firstQuestion = quizZilla.questions[0];
+let currentQuestionIndex = 0; 
 
-// Injecter le texte de la question dans l'emplacement dédit questionContainer.innerText = firstQuestion.text;
-questionContainer.innerText = firstQuestion.text
-// Pour chaque option, créer un bouton et l'ajouter au conteneur
-  firstQuestion.options.forEach(option => {
-  const optionButton = document.createElement('button'); // crée l'élément button html <button></button>
-  optionButton.innerText = option; // on change le text de button <button> le text de option </button>
-  //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html 
-  optionContainer.appendChild(optionButton); // on ajoute le button comme enfant de optionContainer
-}); */
-// Variables pour suivre l'état du quiz
-let currentQuestionIndex = 0; // Commence à la première question
-// Sélection des éléments HTML
-const buttonSuivant = document.querySelector('#next-button');
-//const currentQuestion = quizZilla.questions[currentQuestionIndex]
+let currentQuestion = quizZilla.questions[currentQuestionIndex];
+
+let isSelectedOptionValid = false
+
+const message = document.querySelector('#message')
 
 function loadQuestion(currentQuestionIndex){
-  // Vider le conteneur des options
-  optionContainer.innerHTML = '';
-  // Récupérer la question actuelle
   const currentQuestion = quizZilla.questions[currentQuestionIndex];
-  // Injecter la question dans le HTML
   questionContainer.innerText = currentQuestion.text;
-  // Injecter les options dans le HTML 
+
+  optionContainer.innerHTML = '';
+
+  isSelectedOptionValid = false;
   
   currentQuestion.options.forEach(option => {
     const optionButton = document.createElement('button');
+    optionButton.classList.add('option-button')
     optionButton.innerText = option; 
-    optionButton.classList.add('option-button');
     optionContainer.appendChild(optionButton);
-    
+    buttonSuivant.disabled = true;
 
-    function checkAnswer(optionChoisi, button) {
-      if (optionChoisi === currentQuestion.correct_answer) { 
-        optionButton.style.border = '5px solid green';
-       //alert("correct"); 
-
-      } else {
-        optionButton.style.border = '5px solid red';
-        //alert("incorrect"); 
-      }
-    }
+    // ajouter l'écouteur de click à chaque option pour appeler checkAnswer
     optionButton.addEventListener("click", (event) => {
-      const optionChoisi = event.target.innerText;
-      checkAnswer(optionChoisi, optionButton); // Vérifie si c'est correct et passe le bouton
-  });
+    const optionChoisi = event.target.innerText;
+    checkAnswer(optionChoisi, currentQuestion.correct_answer);
+    buttonSuivant.disabled = false;
     
-  buttonSuivant.disabled = true
-  optionButton.addEventListener("click",(event)=>
-    {
-    const optionChoisi = event.target.innerText
-    console.log(optionChoisi)
-    checkAnswer(optionChoisi)
-    buttonSuivant.disabled = false
-    })
-  })
-}
-loadQuestion(currentQuestionIndex)
-// Ajouter un écouteur d'événements pour le bouton "Suivant"
+    // Désactivé les autres options lorsque une option est choisie
+    const allOptions = optionButton.querySelectorAll('button');
+    allOptions.forEach(btn => {
+      btn.disabled = true;
+      console.log('Options désactivée :', btn.innerText);
+    });
+    });
+  });
+};
+
+// Récupérer le bouton suivant & le bouton replay
+const buttonSuivant = document.querySelector('#next-button');
+const replayButton = document.querySelector('#replay-button');
+
 buttonSuivant.addEventListener('click',() => {
-  // Incrémenter l'index de la question
   currentQuestionIndex++;
+  /*
+  if (isSelectedOptionValid) {
+        pointCount++
+        score.innerHTML =`Score total: ${pointCount}` 
+  };*/
+
   // Vérifier s'il reste des questions
   if (currentQuestionIndex < quizZilla.questions.length) {
-    // Afficher la question suivante
     loadQuestion(currentQuestionIndex)
-  
   } else {
     // Si plus de questions, indiquer la fin du quiz
     questionContainer.innerText = 'Fin du quiz';
-    optionContainer.innerHTML = ''; // Effacer les options
-    buttonSuivant.style.display = 'none'; // Cacher le bouton Suivant
-    replayButton.style.display="inline-block"
+    optionContainer.innerHTML = ''; 
+    buttonSuivant.style.display = 'none'; 
+    replayButton.style.display = "inline-block"
+    score.innerHTML =`Score total: ${pointCount}`
+    if (pointCount === quizZilla.questions.length){
+      message.innerText = "Bien joué !!!"
+    } else if (pointCount >= 0.5*quizZilla.questions.length){
+      message.innerText = "Pas mal !"
+    } else {
+      message.innerText = "Recommence"
+    }
   }
 });
-// Sélection des éléments HTML
-const replayButton = document.querySelector('#replay-button')
-//TODO Ajouter le bouton rejouer à votre liste d‘élements
+
 // Fonction pour réinitialiser le quiz
 replayButton.addEventListener('click', () => {
-  // TODO Réinitialiser l'index 
-    currentQuestionIndex=0 
-    buttonSuivant.style.display='inline-block'
-    replayButton.style.display ='none'
-    loadQuestion(currentQuestionIndex)
-})
-  // TODO Cacher le bouton Rejouer et afficher le bouton Suivant
-r  
-  // TODO Recharger la première question 
+    currentQuestionIndex=0; 
+    buttonSuivant.style.display='inline-block';
+    replayButton.style.display ='none';
+    loadQuestion(currentQuestionIndex);
+    score.innerHTML="";
+    pointCount = 0;
+    message.innerHTML="";
+});
 
-  
+loadQuestion(currentQuestionIndex);
+
+// Affichage du score
+const score = document.querySelector('#score');
+// Système de points
+let pointCount = 0
+
+function checkAnswer(optionChoisi, correct_answer) {
+  isSelectedOptionValid = optionChoisi === currentQuestion.correct_answer
+  if (optionChoisi === correct_answer) {
+    pointCount++;
+    console.log('Bonne réponse')
+    //alert("correct"); 
+  } else {
+    //alert("incorrect"); 
+    console.log('Mauvaise réponse')
+  }
+
+  return score.innerText = `Votre score est de ${pointCount}`
+}; 
