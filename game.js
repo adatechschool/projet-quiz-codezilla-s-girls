f2450aa3f40a58109fd4f22e980c617ed47ece7f
stellab95 <stellab95@hotmail.fr>
2024-11-04 16:40:53 +0100

    test merge

--- game.js
+++ game.js
@@ -4,18 +4,6 @@ import { quizZilla } from './question.js';
 const questionContainer = document.querySelector('.question');
 const optionContainer = document.querySelector('.options');
 
-<<<<<<< HEAD
-let currentQuestionIndex = 0; // Commence à la première question
-let isSelectedOptionValid = false
-
-// Sélection des éléments HTML
-const buttonSuivant = document.querySelector('#next-button');
-//const currentQuestion = quizZilla.questions[currentQuestionIndex]
-const score = document.querySelector('#score')
-let pointCount = 0
-const message = document.querySelector('#message')
-//score.innerHTML = `Score: ${pointCount}`
-=======
 let currentQuestionIndex = 0; 
 
 let currentQuestion = quizZilla.questions[currentQuestionIndex];
@@ -24,23 +12,13 @@ let isSelectedOptionValid = false
 
 const message = document.querySelector('#message')
 
->>>>>>> branche_stella
 function loadQuestion(currentQuestionIndex){
   const currentQuestion = quizZilla.questions[currentQuestionIndex];
   questionContainer.innerText = currentQuestion.text;
 
   optionContainer.innerHTML = '';
-<<<<<<< HEAD
-  isSelectedOptionValid = false
-  // Récupérer la question actuelle
-  const currentQuestion = quizZilla.questions[currentQuestionIndex];
-  // Injecter la question dans le HTML
-  questionContainer.innerText = currentQuestion.text;
-  // Injecter les options dans le HTML 
-=======
 
   isSelectedOptionValid = false;
->>>>>>> branche_stella
   
   currentQuestion.options.forEach(option => {
     const optionButton = document.createElement('button');
@@ -69,36 +47,7 @@ function loadQuestion(currentQuestionIndex){
 const buttonSuivant = document.querySelector('#next-button');
 const replayButton = document.querySelector('#replay-button');
 
-<<<<<<< HEAD
-    function checkAnswer(optionChoisi) {
-      isSelectedOptionValid = optionChoisi === currentQuestion.correct_answer
-      if (optionChoisi === currentQuestion.correct_answer) {
-        alert("correct"); 
-      } else {
-        alert("incorrect"); 
-      }
-    }
-  buttonSuivant.disabled = true
-  optionButton.addEventListener("click",(event)=>
-    {
-    const optionChoisi = event.target.innerText
-    console.log(optionChoisi)
-    checkAnswer(optionChoisi)
-    buttonSuivant.disabled = false
-    })
-  })
-}
-loadQuestion(currentQuestionIndex)
-// Ajouter un écouteur d'événements pour le bouton "Suivant"
 buttonSuivant.addEventListener('click',() => {
-  if (isSelectedOptionValid) {
-        pointCount++
-        score.innerHTML =`Score total: ${pointCount}` 
-  }
-  // Incrémenter l'index de la question
-=======
-buttonSuivant.addEventListener('click',() => {
->>>>>>> branche_stella
   currentQuestionIndex++;
   /*
   if (isSelectedOptionValid) {
@@ -112,13 +61,8 @@ buttonSuivant.addEventListener('click',() => {
   } else {
     // Si plus de questions, indiquer la fin du quiz
     questionContainer.innerText = 'Fin du quiz';
-<<<<<<< HEAD
-    optionContainer.innerHTML = ''; // Effacer les options
-    buttonSuivant.style.display = 'none'; // Cacher le bouton Suivant
-=======
     optionContainer.innerHTML = ''; 
     buttonSuivant.style.display = 'none'; 
->>>>>>> branche_stella
     replayButton.style.display = "inline-block"
     score.innerHTML =`Score total: ${pointCount}`
     if (pointCount === quizZilla.questions.length){
@@ -133,18 +77,6 @@ buttonSuivant.addEventListener('click',() => {
 
 // Fonction pour réinitialiser le quiz
 replayButton.addEventListener('click', () => {
-<<<<<<< HEAD
-  // TODO Réinitialiser l'index 
-    currentQuestionIndex=0 
-    buttonSuivant.style.display='inline-block'
-    replayButton.style.display ='none'
-    loadQuestion(currentQuestionIndex)
-    score.innerHTML=""
-    pointCount = 0 
-    message.innerHTML=""
-})
-
-=======
     currentQuestionIndex=0; 
     buttonSuivant.style.display='inline-block';
     replayButton.style.display ='none';
@@ -174,4 +106,3 @@ function checkAnswer(optionChoisi, correct_answer) {
 
   return score.innerText = `Votre score est de ${pointCount}`
 }; 
->>>>>>> branche_stella
