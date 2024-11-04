bd4d4878d2e50a81aa5997914175b5a671ce0820
stellab95 <stellab95@hotmail.fr>
2024-11-04 10:17:09 +0100

    ajout game.js et index.html

--- game.js
+++ game.js
@@ -2,40 +2,25 @@ import {quizZilla} from './question.js' // Import des questions
 // Récupérer les emplacements pour injecter la question et les options
 const questionContainer = document.querySelector('.question')
 const optionContainer = document.querySelector('.options')
-<<<<<<< HEAD
-// Récupérer la première question
-const firstQuestion = quizZilla.questions[0];
-=======
 
 // Récupérer la première question
 /*const firstQuestion = quizZilla.questions[0];
 
->>>>>>> origin/branche_giau
 // Injecter le texte de la question dans l'emplacement dédit questionContainer.innerText = firstQuestion.text;
 questionContainer.innerText = firstQuestion.text
 // Pour chaque option, créer un bouton et l'ajouter au conteneur
   firstQuestion.options.forEach(option => {
   const optionButton = document.createElement('button'); // crée l'élément button html <button></button>
   optionButton.innerText = option; // on change le text de button <button> le text de option </button>
-<<<<<<< HEAD
-  //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html
-  optionContainer.appendChild(optionButton); // on ajoute le button comme enfant de optionContainer
-});
-=======
   //optionPremierQuestion.classList.add('__________'); // on ajoute une classe à un élément html 
   optionContainer.appendChild(optionButton); // on ajoute le button comme enfant de optionContainer
 }); */
->>>>>>> origin/branche_giau
 // Variables pour suivre l'état du quiz
 let currentQuestionIndex = 0; // Commence à la première question
 // Sélection des éléments HTML
 const buttonSuivant = document.querySelector('#next-button');
-<<<<<<< HEAD
-const currentQuestion = quizZilla.questions[currentQuestionIndex]
-=======
 //const currentQuestion = quizZilla.questions[currentQuestionIndex]
 
->>>>>>> origin/branche_giau
 function loadQuestion(currentQuestionIndex){
   // Vider le conteneur des options
   optionContainer.innerHTML = '';
@@ -43,31 +28,30 @@ function loadQuestion(currentQuestionIndex){
   const currentQuestion = quizZilla.questions[currentQuestionIndex];
   // Injecter la question dans le HTML
   questionContainer.innerText = currentQuestion.text;
-<<<<<<< HEAD
-  // Injecter les options dans le HTML
-  currentQuestion.options.forEach(option => {
-    const optionButton = document.createElement('button');
-    optionButton.innerText = option;
-    // optionContainer.classList.add('option-button');
-    optionContainer.appendChild(optionButton);
-  })
-}
-=======
   // Injecter les options dans le HTML 
   
   currentQuestion.options.forEach(option => {
     const optionButton = document.createElement('button');
     optionButton.innerText = option; 
-    // optionContainer.classList.add('option-button');
+    optionButton.classList.add('option-button');
     optionContainer.appendChild(optionButton);
+    
 
-    function checkAnswer(optionChoisi) {
-      if (optionChoisi === currentQuestion.correct_answer) {
-        alert("correct"); 
+    function checkAnswer(optionChoisi, button) {
+      if (optionChoisi === currentQuestion.correct_answer) { 
+        optionButton.style.border = '5px solid green';
+       //alert("correct"); 
+
       } else {
-        alert("incorrect"); 
+        optionButton.style.border = '5px solid red';
+        //alert("incorrect"); 
       }
     }
+    optionButton.addEventListener("click", (event) => {
+      const optionChoisi = event.target.innerText;
+      checkAnswer(optionChoisi, optionButton); // Vérifie si c'est correct et passe le bouton
+  });
+    
   buttonSuivant.disabled = true
   optionButton.addEventListener("click",(event)=>
     {
@@ -79,7 +63,6 @@ function loadQuestion(currentQuestionIndex){
   })
 }
 loadQuestion(currentQuestionIndex)
->>>>>>> origin/branche_giau
 // Ajouter un écouteur d'événements pour le bouton "Suivant"
 buttonSuivant.addEventListener('click',() => {
   // Incrémenter l'index de la question
@@ -88,10 +71,7 @@ buttonSuivant.addEventListener('click',() => {
   if (currentQuestionIndex < quizZilla.questions.length) {
     // Afficher la question suivante
     loadQuestion(currentQuestionIndex)
-<<<<<<< HEAD
-=======
   
->>>>>>> origin/branche_giau
   } else {
     // Si plus de questions, indiquer la fin du quiz
     questionContainer.innerText = 'Fin du quiz';
@@ -105,43 +85,14 @@ const replayButton = document.querySelector('#replay-button')
 //TODO Ajouter le bouton rejouer à votre liste d‘élements
 // Fonction pour réinitialiser le quiz
 replayButton.addEventListener('click', () => {
-<<<<<<< HEAD
-  // TODO Réinitialiser l'index
-    currentQuestionIndex=0
-=======
   // TODO Réinitialiser l'index 
     currentQuestionIndex=0 
->>>>>>> origin/branche_giau
     buttonSuivant.style.display='inline-block'
     replayButton.style.display ='none'
     loadQuestion(currentQuestionIndex)
 })
   // TODO Cacher le bouton Rejouer et afficher le bouton Suivant
-<<<<<<< HEAD
-  // TODO Recharger la première question
-  function checkAnswer() {
-    optionContainer.addEventListener("click",(event)=>
-    {
-    const optionChoisi = event.target.innerText
-    console.log(optionChoisi)
-    if (optionChoisi === currentQuestion.correct_answer) {
-      alert("correct");
-    } else {
-      alert("incorrect");
-    }
-  })
-}
-  checkAnswer()
-
-
-
-
-
-
-
-=======
   
   // TODO Recharger la première question 
 
   
->>>>>>> origin/branche_giau
